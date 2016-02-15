6f82a83e066e058556cbe3fb420a2f8c3e4714fb
Salamatov Maks <salamatovmaks@gmail.com>
2016-02-15 16:29:32 +0200

    changes_hw_7_8

--- JS_homework_FEo2/homework_7_8/js/script.js
+++ JS_homework_FEo2/homework_7_8/js/script.js
@@ -1,11 +1,38 @@
 $(function () {
+
+	// Task1 HomeWork_7_8 
+
 	var $tabs = $('.tabs_items');
 	var $tabsContent = $('.tabs_content')
+	
 	$tabs.on('click', function(e) {
 		$tabs.removeClass('tabs_items_selected').eq($(this).index()).addClass('tabs_items_selected');
 		e.preventDefault();
-		$tabsContent.hide().eq($(this).index()).addClass('tabs_content_selected');
+		// $tabsContent.removeClass('tabs_content_selected').eq($(this).index()).addClass('tabs_content_selected');
+		$tabsContent.hide().eq($(this).index()).show();	
 	});
+
+	// Task1 HomeWork_7_8
+
+	var $textArea = $('.form_field_block');
+	var $advice = $('.advice_item');
+
+	
+	
+		$textArea.hover(
+			function() {
+				$advice.show();
+			}, 
+			function() {
+				$advice.hide();
+			});
+
+	var $submit = $('.button')
+
+	$submit.on('click', function() {
+		$advice.show();
+	});
+	
 });
 
 
